5c75204859630007540bf20cd03bae90be73c950
Robert <robertinthecloud@icloud.com>
2024-07-29 23:21:00 -0700

    add daisyui

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,67 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main
+      data-theme="amarr"
+      className="flex min-h-screen flex-col items-center gap-5 p-24"
+    >
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input input-bordered w-full max-w-xs"
+      />
+      <div className="flex flex-col items-center gap-2">
+        <div className="flex items-center justify-between gap-2">
+          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-secondary">Cancel</button>
+          <button className="btn btn-accent">Reset</button>
+          <button className="btn btn-warning">Delete</button>
+        </div>
+        <div
+          data-theme="caldari"
+          className="flex items-center justify-between gap-2"
+        >
+          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-secondary">Cancel</button>
+          <button className="btn btn-accent">Reset</button>
+          <button className="btn btn-warning">Delete</button>
+        </div>
+        <div
+          data-theme="gallente"
+          className="flex items-center justify-between gap-2"
+        >
+          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-secondary">Cancel</button>
+          <button className="btn btn-accent">Reset</button>
+          <button className="btn btn-warning">Delete</button>
+        </div>
+        <div
+          data-theme="minmatar"
+          className="flex items-center justify-between gap-2"
+        >
+          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-secondary">Cancel</button>
+          <button className="btn btn-accent">Reset</button>
+          <button className="btn btn-warning">Delete</button>
+        </div>
+        <div
+          data-theme="ore"
+          className="flex items-center justify-between gap-2"
+        >
+          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-secondary">Cancel</button>
+          <button className="btn btn-accent">Reset</button>
+          <button className="btn btn-warning">Delete</button>
+        </div>
+        <div
+          data-theme="sisters"
+          className="flex items-center justify-between gap-2"
+        >
+          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-secondary">Cancel</button>
+          <button className="btn btn-accent">Reset</button>
+          <button className="btn btn-warning">Delete</button>
+        </div>
+      </div>
+    </main>
   );
 }
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,4 +1,10 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+
+const eveThemeDefaults = {
+  "base-100": "#111111",
+  neutral: "#C8C8C1",
+};
 
 const config: Config = {
   content: [
@@ -15,6 +21,69 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      {
+        amarr: {
+          ...eveThemeDefaults,
+          primary: "#A38264",
+          secondary: "#50412B",
+          accent: "FFEE93",
+          "base-200": "#07030D",
+          warning: "#F39058",
+        },
+        caldari: {
+          ...eveThemeDefaults,
+          primary: "#9AD2E3",
+          secondary: "#282827",
+          accent: "#48C7BF",
+          "base-200": "#030E0E",
+          warning: "#DC8C2E",
+        },
+        gallente: {
+          ...eveThemeDefaults,
+          primary: "#58BF9A",
+          secondary: "#344238",
+          accent: "#6DB09E",
+          "base-200": "#0A090F",
+          warning: "#F39058",
+        },
+        minmatar: {
+          ...eveThemeDefaults,
+          primary: "#D05C3B",
+          secondary: "#362A25",
+          accent: "#9D452D",
+          "base-200": "#030909",
+          warning: "#F39058",
+        },
+        ore: {
+          ...eveThemeDefaults,
+          primary: "#DDB825",
+          secondary: "#394961",
+          accent: "#55999C",
+          "base-200": "#030805",
+          warning: "#DE8B78",
+        },
+        sisters: {
+          ...eveThemeDefaults,
+          primary: "#E55252",
+          secondary: "#373837",
+          accent: "#A1DDE0",
+          "base-200": "#070D11",
+          warning: "#C0B337",
+        },
+      },
+    ],
+  },
+  plugins: [daisyui, require("@tailwindcss/typography")],
 };
 export default config;
+
+// amarr #A38264,#FFEE93,#07030D,#F39058
+// caldari #9AD2E3,#48C7BF,#030E0E,#DC8C2E
+// gallente #58BF9A,#6DB09E,#0A090F,#F39058
+// minmatar #D05C3B,#9D452D,#030909,#F39058
+// ore #DDB825,#55999C,#030805,#DE8B78
+// sisters #E55252,#A1DDE0,#070D11,#C0B337
