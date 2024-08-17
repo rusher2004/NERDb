4bd39ae36809ef99f9b6b0c3d3422cb4b5a42ae3
Robert <robertinthecloud@icloud.com>
2024-08-16 18:39:53 -0700

    typo

--- web/app/home/layout.tsx
+++ web/app/home/layout.tsx
@@ -1,4 +1,4 @@
-export const experimenal_ppr = true;
+export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div>{children}</div>;
