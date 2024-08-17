9ea9d05087afb4a55621ee45a882eafde47ee263
Robert <robertinthecloud@icloud.com>
2024-08-17 01:01:34 -0700

    hero styling

--- web/app/ui/Hero/Hero.tsx
+++ web/app/ui/Hero/Hero.tsx
@@ -2,15 +2,11 @@ import Search from "@/app/ui/Search/Search";
 
 export default function Hero() {
   return (
-    <div className="hero h-full">
-      <div className="hero-content text-center text-base-content">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+    <div className="hero size-full">
+      <div className="hero-content text-center text-base-content size-full">
+        <div>
+          <h1 className="text-4xl font-bold">New Eden Rivalries Database</h1>
+          <p className="py-6">{/* {put something nice here} */}</p>
           <Search />
         </div>
       </div>
