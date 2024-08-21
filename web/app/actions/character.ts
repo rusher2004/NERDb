deee74381ac6e0d31ec9c475f2d91e7e4e2b0a9c
Robert <robertinthecloud@icloud.com>
2024-08-21 14:35:24 -0700

    Add Sentry (#28)

--- web/app/actions/character.ts
+++ web/app/actions/character.ts
@@ -1,34 +1,49 @@
 "use server";
 
 import sql from "@/app/lib/db";
+import { headers } from "next/headers";
+import * as Sentry from "@sentry/nextjs";
 import { Character } from "@/app/lib/definitions";
 
 export async function getCharacter(id: number): Promise<Character | null> {
-  try {
-    const [character] = await sql<Character[]>`
-      SELECT
-        birthday,
-        bloodline_id,
-        esi_alliance_id,
-        esi_character_id,
-        esi_corporation_id,
-        description,
-        faction_id,
-        gender,
-        name,
-        race_id,
-        security_status,
-        title
-      FROM
-        player.character
-      WHERE
-        esi_character_id = ${id}
-      LIMIT 1
-    `;
+  const formData = new FormData();
+  formData.append("id", id.toString());
 
-    return character;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
+  return Sentry.withServerActionInstrumentation(
+    "getCharacter",
+    {
+      formData: formData,
+      headers: headers(),
+      recordResponse: true,
+    },
+    async () => {
+      try {
+        const [character] = await sql<Character[]>`
+          SELECT
+            birthday,
+            bloodline_id,
+            esi_alliance_id,
+            esi_character_id,
+            esi_corporation_id,
+            description,
+            faction_id,
+            gender,
+            name,
+            race_id,
+            security_status,
+            title
+          FROM
+            player.character
+          WHERE
+            esi_character_id = ${id}
+          LIMIT 1
+        `;
+
+        return character;
+      } catch (err) {
+        console.error(err);
+        throw new Error(`failed to get character ${id}, ${err}`);
+      }
+    }
+  );
 }
---
+++ web/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://252c73e4f4b709370b75aab2ce14cd31@o4507816175861760.ingest.us.sentry.io/4507816185561088",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
