1d272caa3939f3649448fc42b601365597bf1f68
Robert <robertinthecloud@icloud.com>
2024-08-21 14:12:38 -0700

    add sentry

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+import { withSentryConfig } from '@sentry/nextjs';
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  org: "procyon-innovations",
+  project: "nerdb-web",
+});
---
+++ web/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://252c73e4f4b709370b75aab2ce14cd31@o4507816175861760.ingest.us.sentry.io/4507816185561088",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
