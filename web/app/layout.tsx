7ceb561c2e04059bb92dc5be5bc9cdbc7b14f717
Robert <robertinthecloud@icloud.com>
2024-08-25 15:08:54 -0700

    umamize (#36)

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
+import Head from "next/head";
 import type { Metadata } from "next";
-import NavBar from "@/app/ui/Nav/NavBar";
 import { exo2 } from "@/app/ui/fonts";
+import NavBar from "@/app/ui/Nav/NavBar";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -17,6 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c94d6ed7-21a9-40aa-9cca-4c144b826d3c"
+          key="umami-script"
+        ></script>
+      </Head>
       <body className={`${exo2.className} antialiased`}>
         <main className="min-h-dvh flex flex-col items-center gap-4">
           <NavBar />
