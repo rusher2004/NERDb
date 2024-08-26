ccf7c85264e73f0b1e09ac2a53fb46a02719e109
Robert <robertinthecloud@icloud.com>
2024-08-25 21:15:09 -0700

    use <Script />

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Head from "next/head";
+import Script from "next/script";
 import type { Metadata } from "next";
 import { exo2 } from "@/app/ui/fonts";
 import NavBar from "@/app/ui/Nav/NavBar";
@@ -18,14 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id="c94d6ed7-21a9-40aa-9cca-4c144b826d3c"
-          key="umami-script"
-        ></script>
-      </Head>
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="c94d6ed7-21a9-40aa-9cca-4c144b826d3c"
+        key="umami-script"
+      />
       <body className={`${exo2.className} antialiased`}>
         <main className="min-h-dvh flex flex-col items-center gap-4">
           <NavBar />
